b295bb09122ad6b6c53d3942c22d80ae82c0bbbd
RusticPotato <battaglmb@gmail.com>
2023-10-10 23:22:44 -0400

    test valid and invalid player returns ok

--- tests/test_player_api.py
+++ tests/test_player_api.py
@@ -9,8 +9,22 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 class TestPlayerAPI(TestCase):
 
-	def testPlayerReturns(self):
+	def testValidPlayerReturns200(self):
 		url = "http://localhost:5000/v2/players/score"
 		params = {"name": ["Player1"]}
 		score = requests.get(url, params)
 		assert score.status_code == 200
+
+	def testInvalidPlayerReturns200(self):
+		url = "http://localhost:5000/v2/players/score"
+		params = {"name": [""]}
+		response = requests.get(url, params)
+		assert response.status_code == 200
+	
+	def testInvalidPlayerBodyIsEmpty(self):
+		url = "http://localhost:5000/v2/players/score"
+		params = {"name": [""]}
+		response = requests.get(url, params)
+		error = f"Invalid response return type, expected list[dict]"
+		print(response.json())
+		assert isinstance(response.json(), list), error
