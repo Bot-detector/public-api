c7892c1fb44e25499686ebc4dda0aff6a61b8de0
RusticPotato <battaglmb@gmail.com>
2023-10-10 21:29:49 -0400

    adding pool_recycle to use docker compose env var

--- src/core/database/database.py
+++ src/core/database/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from src.core.config import settings
 
 # Create an async SQLAlchemy engine
-engine = create_async_engine(settings.DATABASE_URL, echo=(settings.ENV != "PRD"))
+engine = create_async_engine(settings.DATABASE_URL, pool_recycle=settings.pool_recycle, echo=(settings.ENV != "PRD"))
 
 # Create a session factory
 SessionFactory = sessionmaker(
