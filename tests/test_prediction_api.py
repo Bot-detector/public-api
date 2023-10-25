99d02509bce8f0a3aae3ddf07d34b6b7aa4943c3
RusticPotato <battaglmb@gmail.com>
2023-10-25 19:42:06 -0400

    black formatting

--- tests/test_prediction_api.py
+++ tests/test_prediction_api.py
@@ -3,31 +3,29 @@ import sys
 import requests
 from unittest import TestCase
 
-
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 class TestPredictionAPI(TestCase):
+    def testValidPredictionReturns200(self):
+        url = "http://localhost:5000/v2/prediction"
+        params = {}
+        # build params
+        params["name"] = ["Player1"]
+        params["breakdown"] = False
+        score = requests.get(url, params)
+        assert score.status_code == 200
 
-	def testValidPredictionReturns200(self):
-		url = "http://localhost:5000/v2/prediction"
-		params = {}
-		# build params
-		params["name"]= ["Player1"]
-		params["breakdown"]= False
-		score = requests.get(url, params)
-		assert score.status_code == 200
+    # def testInvalidPredictionReturns200(self):
+    # 	url = "http://localhost:5000/v2/prediction"
+    # 	params = {"name": [""]}
+    # 	response = requests.get(url, params)
+    # 	assert response.status_code == 200
 
-	# def testInvalidPredictionReturns200(self):
-	# 	url = "http://localhost:5000/v2/prediction"
-	# 	params = {"name": [""]}
-	# 	response = requests.get(url, params)
-	# 	assert response.status_code == 200
-	
-	# def testInvalidPredictionBodyIsEmpty(self):
-	# 	url = "http://localhost:5000/v2/prediction"
-	# 	params = {"name": [""]}
-	# 	response = requests.get(url, params)
-	# 	error = f"Invalid response return type, expected list[dict]"
-	# 	print(response.json())
-	# 	assert isinstance(response.json(), list), error
+    # def testInvalidPredictionBodyIsEmpty(self):
+    # 	url = "http://localhost:5000/v2/prediction"
+    # 	params = {"name": [""]}
+    # 	response = requests.get(url, params)
+    # 	error = f"Invalid response return type, expected list[dict]"
+    # 	print(response.json())
+    # 	assert isinstance(response.json(), list), error
