ec3ec4bdea55edebf48ad8b8c67fc08a644e4500
RusticPotato <battaglmb@gmail.com>
2023-11-01 20:39:02 -0400

    default should be 0 if not given

--- src/app/views/input/report.py
+++ src/app/views/input/report.py
@@ -4,15 +4,15 @@ from pydantic.fields import Field
 
 
 class Equipment(BaseModel):
-    equip_head_id: Optional[int] = Field(None, ge=0)
-    equip_amulet_id: Optional[int] = Field(None, ge=0)
-    equip_torso_id: Optional[int] = Field(None, ge=0)
-    equip_legs_id: Optional[int] = Field(None, ge=0)
-    equip_boots_id: Optional[int] = Field(None, ge=0)
-    equip_cape_id: Optional[int] = Field(None, ge=0)
-    equip_hands_id: Optional[int] = Field(None, ge=0)
-    equip_weapon_id: Optional[int] = Field(None, ge=0)
-    equip_shield_id: Optional[int] = Field(None, ge=0)
+    equip_head_id: Optional[int] = Field(0, ge=0)
+    equip_amulet_id: Optional[int] = Field(0, ge=0)
+    equip_torso_id: Optional[int] = Field(0, ge=0)
+    equip_legs_id: Optional[int] = Field(0, ge=0)
+    equip_boots_id: Optional[int] = Field(0, ge=0)
+    equip_cape_id: Optional[int] = Field(0, ge=0)
+    equip_hands_id: Optional[int] = Field(0, ge=0)
+    equip_weapon_id: Optional[int] = Field(0, ge=0)
+    equip_shield_id: Optional[int] = Field(0, ge=0)
 
 
 class Detection(BaseModel):
