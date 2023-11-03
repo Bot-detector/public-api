b4358cd450cbc68f98b6bdb56d6a9d7aaeaf4267
RusticPotato <3542090+RusticPotatoes@users.noreply.github.com>
2023-11-03 12:59:00 -0400

    Update feedback.py

--- src/app/models/feedback.py
+++ src/app/models/feedback.py
@@ -1,77 +1,49 @@
-import logging
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+from src.app.models.feedback import Feedback
 from src.app.views.input.feedback import FeedbackIn
-from src.core.kafka.engine import AioKafkaEngine
+from src.app.views.response.feedback import FeedbackResponse
+from src.core.database import get_db
+from src.core.fastapi.dependencies.to_jagex_name import to_jagex_name
 
-logger = logging.getLogger(__name__)
+router = APIRouter(tags=["feedback"])
 
-from src.core.database.models.feedback import Feedback as dbFeedback
-
-
-class Feedback:
+@router.get("/feedback/score", response_model=list[FeedbackResponse])
+async def get_feedback_score(
+    names: list[str] = Query(..., max_length=13),  # Update the query parameter
+    db: Session = Depends(get_db),  # Use the SQLAlchemy session dependency
+):
     """
-    Represents a class for retrieving feedback from the database.
+    Get feedback scores for one or more players.
 
     Args:
-        session (AsyncSession): An asynchronous SQLAlchemy session.
+        names (list[str]): List of player names to retrieve feedback scores for.
 
-    Attributes:
-        session (AsyncSession): The asynchronous SQLAlchemy session to use for database operations.
-
-    Methods:
-        get_feedback(player_name: str) -> list[dict]:
-            Fetch feedback for a given player by their name.
-
+    Returns:
+        list[FeedbackResponse]: List of feedback scores for the specified players.
     """
+    feedback = Feedback(db)
 
-    def __init__(self, kafka_engine: AioKafkaEngine, session) -> None:
-        self.kafka_engine = kafka_engine
-        self.session = session
-        pass
+    # Use the to_jagex_name function to get normalized names
+    normalized_names = await asyncio.gather(*[to_jagex_name(name) for name in names])
 
-    async def get_feedback(self, player_names: tuple[str]) -> list[dict]:
-        """
-        Fetch feedback for given players by their names.
+    # Get feedback scores for the normalized names
+    feedback_scores = feedback.get_feedback(player_names=normalized_names)
 
-        Args:
-            player_names (tuple[str]): The names of the players whose feedback is to be retrieved.
+    # Convert the feedback scores to a list of FeedbackResponse objects
+    feedback_response_list = [FeedbackResponse(
+        id=score.id,
+        ts=str(score.ts),  # Convert the timestamp to a string
+        voter_id=score.voter_id,
+        subject_id=score.subject_id,
+        prediction=score.prediction,
+        confidence=score.confidence,
+        vote=score.vote,
+        feedback_text=score.feedback_text,
+        reviewed=score.reviewed,
+        reviewer_id=score.reviewer_id,
+        user_notified=score.user_notified,
+        proposed_label=score.proposed_label,
+    ) for score in feedback_scores]
 
-        Returns:
-            list[dict]: A list of dictionaries containing feedback data.
-
-        """
-        async with self.session:
-            query: select = select(dbFeedback).where(
-                dbFeedback.player_name.in_(player_names)
-            )
-            result = await self.session.execute(query)
-            feedback_data = result.scalars().all()
-
-        return [feedback.to_dict() for feedback in feedback_data]
-
-    def _check_data_size(self, data: list[FeedbackIn]) -> list[FeedbackIn] | None:
-        return None if len(data) > 5000 else data
-
-    def _check_unique_voter(self, data: list[FeedbackIn]) -> list[FeedbackIn] | None:
-        return None if len(set(d.voter_id for d in data)) > 1 else data
-
-    async def parse_data(self, data: list[dict]) -> list[FeedbackIn] | None:
-        """
-        Parse and validate a list of feedback data.
-        """
-        data = self._check_data_size(data)
-        if not data:
-            logger.warning("invalid data size")
-            return None
-
-        data = self._check_unique_voter(data)
-        if not data:
-            logger.warning("invalid unique voter")
-            return None
-        return data
-
-    async def send_to_kafka(self, data: list[FeedbackIn]) -> None:
-        for feedback in data:
-            feedback = feedback.model_dump_json()
-            self.kafka_engine.message_queue.put_nowait(feedback)
-        return
+    return feedback_response_list
