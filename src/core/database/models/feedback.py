508df1f3134d35f533260bb89a445b913434b959
RusticPotato <3542090+RusticPotatoes@users.noreply.github.com>
2023-11-03 13:01:14 -0400

    Update feedback.py

--- src/core/database/models/feedback.py
+++ src/core/database/models/feedback.py
@@ -1,34 +1,28 @@
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-    Float,
-    Text,
-    SmallInteger,
-    TIMESTAMP,
-    ForeignKey,
-)
+from sqlalchemy import Column, Integer, TIMESTAMP, ForeignKey, String, Float, Text, SmallInteger, UniqueConstraint
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-# from sqlalchemy.sql import text
-# from sqlalchemy.orm import relationship
-from src.core.database.database import Base
-
+Base = declarative_base()
 
 class Feedback(Base):
     __tablename__ = "Feedback"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    ts = Column(TIMESTAMP)
+    ts = Column(TIMESTAMP, nullable=False, server_default="CURRENT_TIMESTAMP")
     voter_id = Column(Integer, ForeignKey("Players.id"), nullable=False)
     subject_id = Column(Integer, ForeignKey("Players.id"), nullable=False)
     prediction = Column(String(50), nullable=False)
     confidence = Column(Float, nullable=False)
-    vote = Column(Integer, server_default="0", nullable=False)
+    vote = Column(Integer, nullable=False, server_default="0")
     feedback_text = Column(Text(collation="utf8mb4_0900_ai_ci"))
-    reviewed = Column(SmallInteger, server_default="0", nullable=False)
+    reviewed = Column(SmallInteger, nullable=False, server_default="0")
     reviewer_id = Column(Integer)
-    user_notified = Column(SmallInteger, server_default="0", nullable=False)
+    user_notified = Column(SmallInteger, nullable=False, server_default="0")
     proposed_label = Column(String(50))
 
-    # voter = relationship("Player", foreign_keys=[voter_id])
-    # subject = relationship("Player", foreign_keys=[subject_id])
+    voter = relationship("Player", foreign_keys=[voter_id], back_populates="feedback_given")
+    subject = relationship("Player", foreign_keys=[subject_id], back_populates="feedback_received")
+
+    __table_args__ = (
+        UniqueConstraint('prediction', 'subject_id', 'voter_id', name='Unique_Vote'),
+    )
