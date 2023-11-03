bedf0e44657453636785c261621729d5f0e72dde
RusticPotato <battaglmb@gmail.com>
2023-11-02 22:44:50 -0400

    add feedback route

--- src/api/v2/__init__.py
+++ src/api/v2/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from . import player, prediction, report
+from . import player, prediction, report, feedback
 
 router = APIRouter()
 router.include_router(player.router)
 router.include_router(prediction.router)
 router.include_router(report.router)
+router.include_router(feedback.router)
