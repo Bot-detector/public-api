f305f7b24895139475fe3c18dfa39949b6976e19
RusticPotato <3542090+RusticPotatoes@users.noreply.github.com>
2023-11-06 17:46:02 -0500

    Reports Tests for Public-API (#18)
    * created hypothesis mock for data view
    
    * mock data with dynamic equipment
    
    * working but for some reason invalid data if empty equipment
    
    * ignore vscode settings
    
    * adding logger warnnings to everything
    
    * ouption int and validator to allow none or int
    
    * dictioanry refined for report, status should be 201
    
    * Update test_report_api.py
    
    * remove debug logs
    
    * revert to original
    
    * equipment dictionary if none should be 0
    
    * loud test to get output when debugging
    
    * default should be 0 if not given
    
    * revert to original
    
    * my linux doing ??
    
    * None > 0
    
    * add settings
    
    ---------
    
    Co-authored-by: extreme4all <>

--- src/app/views/input/report.py
+++ src/app/views/input/report.py
@@ -1,17 +1,19 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, validator
 from pydantic.fields import Field
 
 
 class Equipment(BaseModel):
-    equip_head_id: int = Field(None, ge=0)
-    equip_amulet_id: int = Field(None, ge=0)
-    equip_torso_id: int = Field(None, ge=0)
-    equip_legs_id: int = Field(None, ge=0)
-    equip_boots_id: int = Field(None, ge=0)
-    equip_cape_id: int = Field(None, ge=0)
-    equip_hands_id: int = Field(None, ge=0)
-    equip_weapon_id: int = Field(None, ge=0)
-    equip_shield_id: int = Field(None, ge=0)
+    equip_head_id: Optional[int] = Field(None, ge=0)
+    equip_amulet_id: Optional[int] = Field(None, ge=0)
+    equip_torso_id: Optional[int] = Field(None, ge=0)
+    equip_legs_id: Optional[int] = Field(None, ge=0)
+    equip_boots_id: Optional[int] = Field(None, ge=0)
+    equip_cape_id: Optional[int] = Field(None, ge=0)
+    equip_hands_id: Optional[int] = Field(None, ge=0)
+    equip_weapon_id: Optional[int] = Field(None, ge=0)
+    equip_shield_id: Optional[int] = Field(None, ge=0)
 
 
 class Detection(BaseModel):
