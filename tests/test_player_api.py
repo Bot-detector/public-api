84924dc910277134b4cfd90780c5a214237db93a
RusticPotato <battaglmb@gmail.com>
2023-11-10 21:38:22 -0500

    remove duplicate cases, logging consistancy

--- tests/test_player_api.py
+++ tests/test_player_api.py
@@ -27,45 +27,11 @@ class TestPlayerAPI(TestCase):
     # Define a Hypothesis strategy for player names
     player_names_strategy = st.sampled_from(player_names_list)
 
-    def test_valid_player_returns_success(self):
-        url = "http://localhost:5000/v2/player/report/score"
-        params = {}
-        # build params
-        params["name"] = ["player1"]
-        score = requests.get(url, params)
-        assert score.status_code == 200
-
-    def test_invalid_player_returns_success(self):
-        url = "http://localhost:5000/v2/player/report/score"
-        params = {"name": ["abdefg"]}
-        score = requests.get(url, params)
-        assert score.status_code == 200
-
-    def test_valid_player_returns_data(self):
-        url = "http://localhost:5000/v2/player/report/score"
-        params = {}
-        # build params
-        params["name"] = ["player1"]
-        score = requests.get(url, params)
-        json_data = json.loads(score.text)
-        error = f"Invalid response return type, expected list[dict]"
-        # print(len(json_data))
-        assert len(json_data) > 0
-
-    def test_invalid_player_returns_empty(self):
-        url = "http://localhost:5000/v2/player/report/score"
-        params = {}
-        # build params
-        params["name"] = ["abcdefg"]
-        score = requests.get(url, params)
-        # get first element because conversion makes it in a list
-        json_data = json.loads(score.text)
-        # should return a populated dictionary, should be True
-        assert len(json_data) == 0
-
     # multi valid player check returns list[dict]
     @settings(deadline=500)  # Increase the deadline to 500 milliseconds
-    @given(player_names_count_valid=st.lists(player_names_strategy, min_size=1))
+    @given(
+        player_names_count_valid=st.lists(player_names_strategy, min_size=1, max_size=5)
+    )
     def test_get_player_report_score_valid_players_multi(
         self, player_names_count_valid
     ):
@@ -73,8 +39,11 @@ class TestPlayerAPI(TestCase):
             "http://localhost:5000/v2/player/report/score",
             params={"name": player_names_count_valid},
         )
-        # print(f"Test player: {player_names}, Response: {response.json()}")
-        # Check that the response contains feedback for all the specified players
+
+        if response.status_code != 200:
+            print(
+                f"\nTest Data:\n{player_names_count_valid}\nResponse:\n{response.json()}\n"
+            )
         json_data = response.json()
         self.assertEqual(response.status_code, 200),
         assert len(json_data) > 0, "List is empty"
@@ -85,15 +54,17 @@ class TestPlayerAPI(TestCase):
     # invalid player(s) check returns empty list
     @given(
         player_names_count_invalid=st.lists(
-            st.text(min_size=1, max_size=13), min_size=1
+            st.text(min_size=1, max_size=13), min_size=1, max_size=5
         )
     )
     def test_get_player_report_score_invalid_players(self, player_names_count_invalid):
-        # print(f"Test player: {player_names}")
         response = requests.get(
             "http://localhost:5000/v2/player/report/score",
             params={"name": player_names_count_invalid},
         )
-        # print(f"Response: {response.json()}")
+        if response.status_code != 200:
+            print(
+                f"\nTest Data:\n{player_names_count_invalid}\nResponse:\n{response.json()}\n"
+            )
         self.assertEqual(response.status_code, 200),
         assert response.json() == []
