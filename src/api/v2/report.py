d269ff7939b45ecdeadb4bb9c4e48238370eb2f5
extreme4all <40169115+extreme4all@users.noreply.github.com>
2024-05-21 23:20:13 +0200

    bug fix

--- src/api/v2/report.py
+++ src/api/v2/report.py
@@ -17,6 +17,6 @@ async def post_reports(detections: list[Detection]):
     data = await report.parse_data(detections)
     if not data:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="invalid data")
-    logger.debug(f"Working: {len(data}, {data[0]}")
+    logger.debug(f"Working: {len(data)}, {data[0]}")
     await report.send_to_kafka(data)
     return Ok()
